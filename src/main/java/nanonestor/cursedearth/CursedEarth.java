9328a4133e8fa9de883dbe34e55c0983fa0fbaae
nanonestor <briar.thrain@gmail.com>
2024-12-07 01:19:52 -0800

    hotfix for neoforge 1.20.6 version 1.6
    - Fixed some issues because of changes to the vanilla formatting of data tag folders and recipe formatting, in 1.21+

--- src/main/java/nanonestor/cursedearth/CursedEarth.java
+++ src/main/java/nanonestor/cursedearth/CursedEarth.java
@@ -14,6 +14,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
